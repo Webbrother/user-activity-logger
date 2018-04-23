97a2362c62ab28b748ecb3fdf1df816015b53690
Vitaly Komarov <komarov.vitaliy@gmail.com>
2018-04-23 16:15:13 +0400

    LocalStorage commented until decision about further using

--- src/event-collection.js
+++ src/event-collection.js
@@ -1,15 +1,17 @@
-import LocalStorage from './local-storage';
+// LocalStorage using commented until decision about further using
+// import LocalStorage from './local-storage';
 
 export default class EventCollection {
     constructor({requestInterval, commonData}, {api}) {
         this._api = api;
-        this._ls = new LocalStorage();
+        // this._ls = new LocalStorage();
 
         this._events = [];
 
         this._timerId = window.setInterval(() => {
-            const cachedEvents = this._ls.get('events') || [];
-            const sentEvents = [...cachedEvents, ...this._events];
+            // const cachedEvents = this._ls.get('events') || [];
+            // const sentEvents = [...cachedEvents, ...this._events];
+            const sentEvents = [...this._events];
 
             if (!sentEvents.length) return;
 
@@ -18,10 +20,11 @@ export default class EventCollection {
             this._api.post({
                 commonData,
                 events: sentEvents
-            }).then(r => {
-                this._ls.remove('events');
-            }).catch(e => {
-                this._ls.set('events', sentEvents);
+            })
+            // .then(r => { this._ls.remove('events'); })
+            .catch(e => {
+                // this._ls.set('events', sentEvents);
+                this._events =  [...sentEvents, ...this._events];
             });
         }, requestInterval);
     }
@@ -32,6 +35,6 @@ export default class EventCollection {
 
     destroy() {
         window.clearInterval(this._timerId);
-        this._ls.remove('events');
+        // this._ls.remove('events');
     }
 }

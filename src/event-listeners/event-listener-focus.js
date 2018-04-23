a18b991cbb6763c05e7d568ff20586a6ceba42eb
Vitaly Komarov <komarov.vitaliy@gmail.com>
2018-04-23 16:25:35 +0400

    typo

--- src/event-listeners/event-listener-focus.js
+++ src/event-listeners/event-listener-focus.js
@@ -1,12 +1,12 @@
 import EventListener from './event-listener';
 
-export default class EventListenerClick extends EventListener {
+export default class EventListenerFocus extends EventListener {
     constructor(config, dependencies) {
         super(config, dependencies);
     }
 
     handleEvent(event) {
-        // Ignore focuse on window
+        // Ignore focus on window
         if (event.target === window) {
             event.stopPropagation();
             return;
